89018559076e2af37ae5739f32bc203430b140f5
William Tran <wtran29@gmail.com>
2023-07-28 12:49:32 -0700

    added handlers to repo

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -5,4 +5,9 @@ import "github.com/wtran29/go-rss/internal/models"
 type DatabaseRepo interface {
 	CreateUser(u models.User) (models.User, error)
 	GetUserByAPIKey(apiKey string) (models.User, error)
+	CreateFeed(f models.Feed) (models.Feed, error)
+	GetFeeds() ([]models.Feed, error)
+	CreateFeedFollow(ff models.FeedFollow) (models.FeedFollow, error)
+	GetFeedFollowsByUserID(uid int) ([]models.FeedFollow, error)
+	DeleteFeedFollow(id, uid int) error
 }
