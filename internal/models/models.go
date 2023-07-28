c78b81772b37a285fa6cee16af049fffa16c6c84
William Tran <wtran29@gmail.com>
2023-07-28 12:50:11 -0700

    updated feed follows and feed model

--- internal/models/models.go
+++ internal/models/models.go
@@ -13,7 +13,16 @@ type User struct {
 type Feed struct {
 	ID        int       `json:"id"`
 	Name      string    `json:"name"`
+	URL       string    `json:"url"`
 	UserID    int       `json:"user_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type FeedFollow struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"user_id"`
+	FeedID    int       `json:"feed_id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
