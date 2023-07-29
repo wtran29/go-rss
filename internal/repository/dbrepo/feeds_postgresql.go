7ed2fd939825f486d669990c138178ec79aa8304
William Tran <wtran29@gmail.com>
2023-07-28 23:31:03 -0700

    added a function get the next feed to fetch for scraper

--- internal/repository/dbrepo/feeds_postgresql.go
+++ internal/repository/dbrepo/feeds_postgresql.go
@@ -82,3 +82,83 @@ func (m *postgresDBRepo) GetFeeds() ([]models.Feed, error) {
 	}
 	return feeds, nil
 }
+
+func (m *postgresDBRepo) GetNextFeedsToFetch(limit int) ([]models.Feed, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `SELECT id, name, url, user_id, created_at, updated_at, last_fetched_at FROM feeds
+				ORDER BY last_fetched_at ASC NULLS FIRST LIMIT $1`
+
+	var feeds []models.Feed
+
+	rows, err := m.DB.QueryContext(ctx, query, limit)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var feed models.Feed
+		err := rows.Scan(
+			&feed.ID,
+			&feed.Name,
+			&feed.URL,
+			&feed.UserID,
+			&feed.CreatedAt,
+			&feed.UpdatedAt,
+			&feed.LastFetchedAt,
+		)
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+		feeds = append(feeds, feed)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return feeds, nil
+}
+
+func (m *postgresDBRepo) MarkFeedsAsFetched(id int) (models.Feed, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `UPDATE feeds SET last_fetched_at = $1, updated_at = $2 WHERE id = $3`
+
+	_, err := m.DB.ExecContext(ctx, stmt,
+		time.Now(),
+		time.Now(),
+		id,
+	)
+	if err != nil {
+		log.Println(err)
+		return models.Feed{}, err
+	}
+
+	stmt = `SELECT * FROM feeds WHERE id = $1`
+
+	var feed models.Feed
+
+	err = m.DB.QueryRowContext(ctx, stmt, id).Scan(
+		&feed.ID,
+		&feed.Name,
+		&feed.URL,
+		&feed.UserID,
+		&feed.CreatedAt,
+		&feed.UpdatedAt,
+		&feed.LastFetchedAt,
+	)
+	if err != nil {
+		log.Println(err)
+		return feed, err
+	}
+
+	return feed, nil
+}
